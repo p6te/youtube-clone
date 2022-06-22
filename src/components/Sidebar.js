d2909b5a5c9828fde643b8d51183eed3fe00f203
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-22 15:43:58 +0200

    layout of sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,77 +8,153 @@ import LibraryIcon from "../icons/sidebar/LibraryIcon";
 import LikedIcon from "../icons/sidebar/LikedIcon";
 import ShortsIcon from "../icons/sidebar/ShortsIcon";
 import SubscriptionIcon from "../icons/sidebar/SubscriptionsIcon";
+import VideoIcon from "../icons/sidebar/VideoIcon";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="main sidebar__section ">
-        <div className="home sidebar__row">
+      <ul className="main sidebar__section ">
+        <li className="home sidebar__row">
           <div className="icon ">
             <MainIcon active="" />
           </div>
           <span>Główna</span>
-        </div>
-        <div className="home sidebar__row ">
-          <div className="icon">
-            <ClockIcon active="" />
-          </div>
-          <span>Odkrywaj</span>
-        </div>
-        <div className="home sidebar__row">
+        </li>
+        <li className="home sidebar__row ">
           <div className="icon">
             <DiscoverIcon active="" />
           </div>
-          <span>Shorts</span>
-        </div>
-        <div className="home sidebar__row">
-          <div className="icon">
-            <HistoryIcon active="" />
-          </div>
-          <span>Subskrypcje</span>
-        </div>
-        <div className="home sidebar__row">
-          <div className="icon">
-            <HistoryIcon active="" />
-          </div>
-          <span>Subskrypcje</span>
-        </div>
-        <div className="home sidebar__row">
-          <div className="icon">
-            <LibraryIcon active="" />
-          </div>
-          <span>Subskrypcje</span>
-        </div>
-        <div className="home sidebar__row">
-          <div className="icon">
-            <LikedIcon active="" />
-          </div>
-          <span>Subskrypcje</span>
-        </div>
-        <div className="home sidebar__row">
+          <span>Odkrywaj</span>
+        </li>
+        <li className="home sidebar__row">
           <div className="icon">
             <ShortsIcon active="" />
           </div>
-          <span>Subskrypcje</span>
-        </div>
-        <div className="home sidebar__row">
+          <span>Shorts</span>
+        </li>
+        <li className="home sidebar__row">
           <div className="icon">
             <SubscriptionIcon active="" />
           </div>
           <span>Subskrypcje</span>
-        </div>
-        <div className="home sidebar__row">
+        </li>
+      </ul>
+
+      <ul className="library sidebar__section ">
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LibraryIcon active="" />
+          </div>
+          <span>Biblioteka</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <HistoryIcon active="" />
+          </div>
+          <span>Historia</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <VideoIcon />
+          </div>
+          <span>Twoje filmy</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <ClockIcon active="" />
+          </div>
+          <span>Do obejrzenia</span>
+        </li>
+        <li className="home sidebar__row">
           <div className="icon">
             <LikedIcon active="" />
           </div>
-          <span>Subskrypcje</span>
-        </div>
+          <span>Polubione filmy</span>
+        </li>
+      </ul>
+
+      <ul className="subscriptions sidebar__section">
+        <h3>SUBSKRYPCJE</h3>
+        <li className="home sidebar__row">
+          <div className="icon-channel"></div>
+          <span>Projekt storny yt</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon-channel"></div>
+          <span>Projekt storny yt</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon-channel"></div>
+          <span>Projekt storny yt</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon-channel"></div>
+          <span>Projekt storny yt</span>
+        </li>
+      </ul>
+      <ul className="more-yt sidebar__section ">
+        <h3>Więcej z youtube</h3>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Youtube Premium</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Filmy</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Gry</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Na żywo</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Sport</span>
+        </li>
+      </ul>
+      <ul className="settings sidebar__section">
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Ustawienia</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Historia raportów</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Pomoc</span>
+        </li>
+        <li className="home sidebar__row">
+          <div className="icon">
+            <LikedIcon active="" />
+          </div>
+          <span>Prześlij opinię</span>
+        </li>
+      </ul>
+      <div className="info sidebar__section ">
+        <span>To jest kopia głównej strony youtube</span>
+        <span>Projekt stworzony w ramach praktyki, bez celów zarobkowych</span>
       </div>
-      <div className="library sidebar__section "></div>
-      <div className="subscriptions sidebar__section"></div>
-      <div className="more-yt sidebar__section "></div>
-      <div className="settings sidebar__section"></div>
-      <div className="info sidebar__section "></div>
     </div>
   );
 }
--- src/icons/sidebar/VideoIcon.js
+++ src/icons/sidebar/VideoIcon.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const VideoIcon = ({ active = false }) => {
+const VideoIcon = () => {
   return (
     <svg
       viewBox="0 0 24 24"
@@ -10,35 +10,13 @@ const VideoIcon = ({ active = false }) => {
       style={{ display: "block", width: "100%", height: " 100%", fill: "#fff" }}
     >
       <g className="style-scope yt-icon">
-        {active === "videoIcon" ? (
-          <path
-            d="M4,10V21h6V15h4v6h6V10L12,3Z"
-            className="style-scope yt-icon  active-icon"
-          ></path>
-        ) : (
-          <path
-            d="M12,4.33l7,6.12V20H15V14H9v6H5V10.45l7-6.12M12,3,4,10V21h6V15h4v6h6V10L12,3Z"
-            className="style-scope yt-icon"
-          ></path>
-        )}
+        <path
+          d="M10,8l6,4l-6,4V8L10,8z M21,3v18H3V3H21z M20,4H4v16h16V4z"
+          className="style-scope yt-icon"
+        ></path>
       </g>
     </svg>
   );
 };
 
 export default VideoIcon;
-
-<svg
-  viewBox="0 0 24 24"
-  preserveAspectRatio="xMidYMid meet"
-  focusable="false"
-  class="style-scope yt-icon"
-  style="pointer-events: none; display: block; width: 100%; height: 100%;"
->
-  <g class="style-scope yt-icon">
-    <path
-      d="M10,8l6,4l-6,4V8L10,8z M21,3v18H3V3H21z M20,4H4v16h16V4z"
-      class="style-scope yt-icon"
-    ></path>
-  </g>
-</svg>;
